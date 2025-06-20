7b5f0af0d46a4e8a1dd32161c397d189e859075b
Tyler <tvanden@live.com>
2025-06-19 23:27:47 -0600

    update to v0.2.1

--- main.go
+++ main.go
@@ -62,7 +62,7 @@ var embeddedDb []byte
 // The main function :p (The more comments the better!)
 func main() {
 	// Version number
-	versionNumber := "v0.2.0"
+	versionNumber := "v0.2.1"
 
 	// Create a temporary file to hold the embedded database
 	tmpFile, err := os.CreateTemp("", "kjv.db")
